91d648e88d1a81f3c2c01356732b8055d310fcd5
VishalYa6 <vy372984@gmail.com>
2024-02-15 10:12:33 +0530

    Added my project

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'kk649teo',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemaTypes/heroImages.ts
@@ -0,0 +1,17 @@
+export default{
+    name: 'heroImage',
+    type:'document',
+    title:'Two hero Images',
+    fields: [
+        {
+            name : 'image1',
+            type: 'image',
+            title: 'First_Image',
+        },
+        {
+            name : 'image2',
+            type :  'image',
+            title : 'Second_Image',
+        }
+    ]
+}
